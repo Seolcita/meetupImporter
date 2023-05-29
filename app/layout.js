9084dc2bac32ecad27aa98799deb294b46bda23c
Seol Hi Kim <seolhikim@Seols-MacBook-Pro.local>
2023-05-28 23:15:56 -0600

    Created Next app

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+/** @format */
+
+export const metadata = {
+  title: 'Meetup Importer',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Meetup Importer</h1>
+    </main>
+  );
+}
